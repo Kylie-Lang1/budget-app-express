9786e0aa53b09371ab3eb096647f2b8cb0704825
Kylie-Lang1 <kylielang@pursuit.org>
2023-01-10 15:49:56 -0500

    added to seed data object

--- models/transactions.js
+++ models/transactions.js
@@ -5,7 +5,8 @@ module.exports = [
         id: crypto.randomUUID(),
         item_name: "savings",
         amount: 500,
-        date: "01/01/23",
+        date: "2023-01-01T05:00:00.000Z",
+        date_formatted: "01/01/23",
         from: "bank",
         category: "savings"
     }
