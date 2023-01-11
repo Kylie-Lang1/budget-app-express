5ecf9c35bd0d2fb604d4b2f27142251a1cdb23db
Kylie-Lang1 <kylielang@pursuit.org>
2023-01-11 04:18:05 -0500

    Added to data object

--- models/transactions.js
+++ models/transactions.js
@@ -5,6 +5,7 @@ module.exports = [
         id: crypto.randomUUID(),
         item_name: "savings",
         amount: 500,
+        action: "deposit",
         date: "2023-01-01T05:00:00.000Z",
         date_formatted: "1/1/23",
         from: "bank",
