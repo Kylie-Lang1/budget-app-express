c470cba1fc2e0f24e15d34e97b2893a649dbfad1
Kylie-Lang1 <kylielang@pursuit.org>
2023-01-11 03:40:30 -0500

    final features

--- models/transactions.js
+++ models/transactions.js
@@ -6,7 +6,7 @@ module.exports = [
         item_name: "savings",
         amount: 500,
         date: "2023-01-01T05:00:00.000Z",
-        date_formatted: "01/01/23",
+        date_formatted: "1/1/23",
         from: "bank",
         category: "savings"
     }
